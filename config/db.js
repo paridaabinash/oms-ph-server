ab3ce96c2d31d488fde6c38e745d8d4f16b2966e
paridaabinash <abinashparida833@gmail.com>
2024-11-16 10:03:49 +0530

    couchdb url change

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const nano = require('nano');
 const username = process.env.COUCHDB_USER;
-const password = process.env.COUCHDB_PASS;
+const password = process.env.COUCHDB_PASSWORD;
 const couchDBUrl = process.env.COUCHDB_URL || `http://${username}:${password}@localhost:5984`; //`http://${username}:${password}@localhost:5984`; // Update this with your CouchDB URL
 const dbName = 'whiteeagle'; // Your CouchDB database name
 const db = nano(couchDBUrl).use(dbName);
