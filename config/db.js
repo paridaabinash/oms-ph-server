4c8b9196fd13b452d6ff5e1f8d4372f404738a36
paridaabinash <abinashparida833@gmail.com>
2024-11-18 11:57:08 +0530

    couchdb pass hashed

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
-const auth = require('./authController')
 const nano = require('nano');
-const couchDBUrl = `https://${process.env.COUCHDB_USER}:${await auth.hashPassword(process.env.COUCHDB_PASSWORD)}@${process.env.COUCHDB_URL}`
-    || `https://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@localhost:5984`; //`http://${username}:${password}@localhost:5984`; // Update this with your CouchDB URL
+const couchDBUrl = process.env.COUCHDB_URL
+    ? `https://${process.env.COUCHDB_USER}:${process.env.COUCHDB_HPASSWORD}@${process.env.COUCHDB_URL}`
+    : `https://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@localhost:5984`;
+
 const dbName = 'whiteeagle'; // Your CouchDB database name
 const db = nano(couchDBUrl).use(dbName);
 
