32092e6619d00438fc3c6c72dcf7f9daddc38ba6
paridaabinash <abinashparida833@gmail.com>
2025-02-26 13:03:54 +0530

    connection address changed

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const nano = require('nano');
 const couchDBUrl =
     //process.env.PROD == true ?
-    `https://${process.env.COUCHDB_USER}:${process.env.COUCHDB_HPASSWORD}@${process.env.COUCHDB_URL}` 
+    `http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@${process.env.COUCHDB_URL}` 
     //`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@localhost:5984`;
 
 const dbName = 'we';
